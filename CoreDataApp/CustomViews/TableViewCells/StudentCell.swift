9a40548c2032a61a97364be727b0956a1364a2d6
Francesca <fharo@belatrixsf.com>
2019-01-07 15:02:03 -0500

    Create student

--- CoreDataApp/CustomViews/TableViewCells/StudentCell.swift
+++ CoreDataApp/CustomViews/TableViewCells/StudentCell.swift
@@ -9,16 +9,14 @@
 import UIKit
 
 class StudentCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+  
+  
+  @IBOutlet weak var txtName: UILabel!
+  @IBOutlet weak var txtAge: UILabel!
+  
+  @IBAction func btnDelete(_ sender: UIButton) {
+  }
+  @IBAction func btnEdit(_ sender: UIButton) {
+  }
+  
 }
--- CoreDataApp/ViewControllers/StudentViewController.swift
+++ CoreDataApp/ViewControllers/StudentViewController.swift
@@ -10,6 +10,47 @@ import UIKit
 
 class StudentViewController: UIViewController {
 
+  
+  @IBOutlet weak var tableView: UITableView!
+  
+  @IBAction func btnAdd(_ sender: UIBarButtonItem) {
+    
+    let alert = UIAlertController(title: "Student List", message: "Add a new student", preferredStyle: UIAlertController.Style.alert)
+    
+    let saveAction = UIAlertAction(title: "Save", style: .default) { (alertAction) in
+      let textField = alert.textFields![0] as UITextField
+      let textField2 = alert.textFields![1] as UITextField    }
+    
+    alert.addTextField { (textField) in
+      textField.placeholder = "Name"
+    }
+    
+    alert.addTextField { (textField2) in
+      textField2.placeholder = "Age"
+    }
+    
+    let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+    
+    alert.addAction(cancelAction)
+    alert.addAction(saveAction)
+    present(alert, animated: true)
+    
+//
+//      let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+//      ac.addTextField()
+//
+//      let submitAction = UIAlertAction(title: "Submit", style: .default) { [unowned ac] _ in
+//        let answer = ac.textFields![0]
+//        let answer2 = ac.textFields![1]
+//
+//        // do something interesting with "answer" here
+//      }
+//
+//      ac.addAction(submitAction)
+//
+//      present(ac, animated: true)
+//
+  }
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
